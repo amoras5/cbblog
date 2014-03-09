52f4781ea0e8d6e56c7fd54e75c901053e4d151c
arturo <arturo.moras@gmail.com>
2014-03-09 13:54:57 -0600

    initial commit

---
+++ app/controllers/password_resets_controller.rb
@@ -0,0 +1,36 @@
+class PasswordResetsController < ApplicationController
+  
+  def new
+  end
+
+  def create
+  	user = User.find_by_email(params[:email])
+<<<<<<< HEAD
+    if user
+  	  user.send_password_reset
+      redirect_to root_path, :notice => "Email enviado con instrucciones."
+    else
+      redirect_to new_password_reset_path, :notice => "No existe ningun usuario con ese email registrado en este blog."
+    end
+=======
+  	user.send_password_reset if user
+  	redirect_to root_path, :notice => "Email enviado con instrucciones."
+>>>>>>> heroku/master
+  end
+
+  def edit
+  	@user = User.find_by_password_reset_token!(params[:id])
+  end
+
+  def update
+  	@user = User.find_by_password_reset_token!(params[:id])
+  	if @user.password_reset_sent_at < 24.hours.ago
+  	  redirect_to new_password_reset_path, :alert => "El tiempo para editar tu contrasena ha explirado."
+  	elsif @user.update_attributes(params[:user])
+  	  redirect_to root_path, :notice => "La Contrasena ha sido cambiada!"
+  	else
+  	  render :edit
+  	end
+  end
+
+end
